3c22ab866a0b10d19fee4309d73d677bc4776f39
abinesh-abi <abinesh1729@gmail.com>
2025-12-30 15:09:58 +0530

    feat: config update

--- config/CONFIG.ts
+++ config/CONFIG.ts
@@ -1,6 +1,6 @@
 export default {
-  REDIS_HOST: process.env.REDIS_HOST,
-  REDIS_PORT: Number(process.env.REDIS_PORT),
+  REDIS_HOST: process.env.REDIS_HOST || 'localhost',
+  REDIS_PORT: Number(process.env.REDIS_PORT) || 6379,
   REDIS_PASSWORD: process.env.REDIS_PASSWORD || undefined,
 
   SERVER_URL: process.env.SERVER_URL || "http://localhost:3000",
